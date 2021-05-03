b07f6fd9fb723666b2b14806ec3d06fa3af7e499
Ayeah Godlove <ayeahgodlove5@gmail.com>
2021-05-03 09:41:20 +0100

    Added DbContext and DbSet

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using CommandAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -15,6 +16,7 @@ namespace CommandAPI {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices (IServiceCollection services) {
             services.AddControllers ();
+            services.AddDbContext<CommandContext> ();
             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo> ();
         }
 
